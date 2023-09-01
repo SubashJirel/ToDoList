387fd3aad7179ab398368473bcb0dddf7dc4325c
Subash Jirel <subashjrl@gmail.com>
2023-09-01 16:13:40 +0545

    initialzed the files

---
+++ main.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+//testing our webpack initial setup of index.js
+console.log('testing index.js webpack');
+//testing out DOM manipulation via webpack
+const contentDiv = document.querySelector('.content');
+
+const h1 = document.createElement('h1');
+h1.textContent = ' hello from index.js';
+contentDiv.appendChild(h1);
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
