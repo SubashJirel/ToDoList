b7ac49e7e2a2518bdeacc0184f618399ed98bb43
Subash Jirel <subashjrl@gmail.com>
2023-09-08 16:16:54 +0545

    created a project module, which will have a projectList as an array that will store all the projects from local storage and if local storage is empty then 2 default projects will be inserted with some tasks. Also added add project and delete project functionality, you have to later change the code to render the dom after adding or deleting the project

---
+++ src/projects.js
@@ -0,0 +1,70 @@
+const projects = (() => {
+  let projectsList = [];
+
+  // GET DEFAULT PROJECTS AND TASKS FROM LOCAL STORAGE
+  if (localStorage.getItem('projects') === null) {
+    projectsList = [
+      {
+        title: 'default Project',
+        tasks: [
+          {
+            title: 'This is a sample task',
+            description: 'some short description',
+            date: '2000-01-01',
+            priority: 'low',
+            projectIndex: 0,
+            taskIndex: 0,
+            completed: false,
+          },
+        ],
+      },
+      {
+        title: 'default workout project',
+        tasks: [
+          {
+            title: 'Do regular home workout',
+            description: 'Just do it',
+            date: '2023-01-01',
+            priority: 'high',
+            projectIndex: 1,
+            taskIndex: 0,
+            completed: false,
+          },
+        ],
+      },
+    ];
+  } else {
+    const projectsFromStorage = JSON.parse(localStorage.getItem('projects'));
+    projectsList = projectsFromStorage;
+  }
+
+  class Project {
+    constructor(title) {
+      this.title = title;
+      this.tasks = [];
+    }
+  }
+
+  function addProject(title) {
+    const project = new Project(title);
+    projectsList.push(project);
+    //after you have added project you have to render later
+    // dom.showProjects();
+  }
+
+  function deleteProject(index) {
+    if (index > -1) {
+      projectsList.splice(index, 1);
+    }
+    //after you have deleted project from the projectList you have to render later
+    // dom.showProjects();
+  }
+
+  return {
+    projectsList,
+    addProject,
+    deleteProject,
+  };
+})();
+
+export default projects;
